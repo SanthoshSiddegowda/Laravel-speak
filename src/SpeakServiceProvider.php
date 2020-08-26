a01e37b7b2d7851ba9ff96d103ac674063817137
santoshashgowda@gmail.com <santhosh.js@goomo.com>
2020-08-27 00:55:26 +0530

    service provider updated

--- src/SpeakServiceProvider.php
+++ src/SpeakServiceProvider.php
@@ -6,44 +6,25 @@ use Illuminate\Support\ServiceProvider;
 
 class SpeakServiceProvider extends ServiceProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
-
-    /**
-     * The commands to be registered from the package.
-     *
-     * @var array
-     */
-    protected $commands = [
-    ];
-
-    /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
     
-    }
-
     /**
-     * Register any package services.
+     * Register any application services.
      *
      * @return void
      */
     public function register()
     {
-        $this->app->bind('passgenerator', function ($app) {
-            return new PassGenerator($app);
-        });
-
-        $this->commands($this->commands);
+        //
     }
 
-    
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
 }
